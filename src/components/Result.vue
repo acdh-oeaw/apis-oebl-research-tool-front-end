5d9aafe67ac099fb33f9d946da847b7777830b79
arnold-graf <arnold.graf@gmail.com>
2020-10-23 14:42:13 +0200

    create results page

---
+++ src/components/Result.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+
+@Component
+export default class Result extends Vue {
+  @Prop() prop: string|null
+  mounted() {
+    
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
