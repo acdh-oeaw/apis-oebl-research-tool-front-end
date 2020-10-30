958b4f3979221a39be709d9392a22396feef5236
arnold-graf <arnold.graf@gmail.com>
2020-10-30 17:18:00 +0100

    person result type (preliminary)

--- src/service/backend.ts
+++ src/service/backend.ts
@@ -10,6 +10,21 @@ interface ApiSuccessMessage {
   success: string
 }
 
+export interface ApiPersonResult {
+  geburtstag: string|null
+  gnd: string
+  label: string|null
+  loc: string|null
+  obv_counts: number
+  obv_entries: string[]
+  query_name: string
+  todestag: string|null
+  viaf: string|null
+  'wien wiki': string|null
+  wikipedia_edits: number
+  wikipedia_txt: string|null
+}
+
 export async function postList(lemmas: ApiPerson[], email: string): Promise<ApiSuccessMessage> {
   const r = await (await fetch(host + '/lemmaresearch/', {
     method: 'POST',
@@ -21,9 +36,9 @@ export async function postList(lemmas: ApiPerson[], email: string): Promise<ApiS
   return r
 }
 
-export async function getResultsForSearch(id: string): Promise<any> {
+export async function getResultsForSearch(id: string): Promise<ApiPersonResult[]> {
   const r = await (await fetch(host + '/lemmaresearch/' + id, {
     method: 'GET'
-  })).json()
-  return r
+  })).json() as { count: number, results: ApiPersonResult[] }
+  return r.results
 }
