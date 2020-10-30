503336e8a090ec723bdf95329009185d7039c998
arnold-graf <arnold.graf@gmail.com>
2020-10-30 17:16:20 +0100

    more useful progress display

--- src/components/QueryProgress.vue
+++ src/components/QueryProgress.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="outer-bar text-left">
-    <div class="bar" :style="{width: progress.found / this.searchTable.length * 100 + '%', background: 'green'}">
+    <div :title="progress.found + ' gefunden'" class="bar" :style="{width: progress.found / this.searchTable.length * 100 + '%', background: '#4caf50'}">
       {{ progress.found }} gefunden
     </div>
-    <div class="bar" :style="{width: progress.ambiguous / this.searchTable.length * 100 + '%', background: 'red'}">
+    <div :title="progress.ambiguous + ' mehrdeutig'" class="bar" :style="{width: progress.ambiguous / this.searchTable.length * 100 + '%', background: '#f44236'}">
       {{ progress.ambiguous }} mehrdeutig
     </div>
-    <div class="bar" :style="{width: progress.notFound / this.searchTable.length * 100 + '%', background: '#ccc'}">
+    <div :title="progress.notFound + ' nicht gefunden'" class="bar" :style="{width: progress.notFound / this.searchTable.length * 100 + '%', background: '#ababab'}">
       {{ progress.notFound }} nicht gefunden
     </div>
   </div>
@@ -22,9 +22,9 @@ export default class Progress extends Vue {
 
   get progress(): {found: number, notFound: number, ambiguous: number} {
     return {
-      found: this.searchTable.filter(r => r.candidateSelected > -1).length,
+      found: this.searchTable.filter(r => r.loaded === true && r.candidateSelected > -1).length,
       notFound: this.searchTable.filter(r => r.loaded === true && r.lobid.length === 0).length,
-      ambiguous: this.searchTable.filter(r => (r.candidateSelected === -1 && r.lobid.length > 1)).length
+      ambiguous: this.searchTable.filter(r => r.loaded === true && r.candidateSelected === -1 && r.lobid.length !== 0).length,
     }
   }
 
@@ -32,14 +32,15 @@ export default class Progress extends Vue {
 </script>
 <style lang="stylus" scoped>
 .outer-bar{
-  background: #ccc
+  background: repeating-linear-gradient(45deg, #ccc, #ccc 10px, #a5a5a5 10px, #a5a5a5 20px)
   border-radius 5px
   overflow hidden
   position relative
   height: 20px
 }
 .bar{
-  text-overflow: ellipsis;
+  border-right: 1px solid rgba(255,255,255,.15)
+  text-overflow: ellipsis
   white-space: nowrap;
   padding: 0 0.5em;
   transition .25s width
