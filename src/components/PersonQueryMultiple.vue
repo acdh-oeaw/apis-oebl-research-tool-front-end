5853380b4cca6eb14c8a262c1cb5c1fe4171a7ac
arnold-graf <arnold.graf@gmail.com>
2020-10-30 17:17:14 +0100

    success overlay, email required and validated, layout tweaks

--- src/components/PersonQueryMultiple.vue
+++ src/components/PersonQueryMultiple.vue
@@ -1,8 +1,39 @@
 <template>
-  <v-container fill-height class="align-stretch">
-    <v-row style="height: 20%; overflow: auto" class="flex-shrink-1">
+  <v-container fill-height class="align-stretch pa-0 pb-5">
+    <v-dialog
+      persistent
+      :max-width="500"
+      :value="successId !== null">
+      <v-card class="text-center">
+        <v-card-title class="text-center overline d-block">
+          Daten zur Verarbeitung gesendet
+        </v-card-title>
+        <v-card-text class="text-center">
+          <p>
+            Die Lemmaliste wurde zur weiteren Verarbeitung an den Server gesendet.
+            Die Ergebnisse werden nach dem Datenabgleich unter dieser Adresse angezeigt:
+          </p>
+          <p>
+            <a :href="'https://'+ host + '/' + successId">{{ host }}/result/{{ successId }}</a>
+          </p>
+          <p>
+            Sie erhalten eine E-Mail an <b>{{ eMail }}</b> mit dem Link, sobald die Verarbeitung abgeschlossen ist.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    <v-form
+      class="row flex-shrink-1"
+      style="height: 15%; overflow: auto;"
+      ref="form"
+      v-model="isFormValid"
+      lazy-validation>
       <v-col cols="3">
-        <v-text-field v-model="eMail" label="E-Mail Adresse" type="email" />
+        <v-text-field
+          v-model="eMail"
+          label="E-Mail Adresse"
+          :rules="[ (e) => this.isNormalEmail(e) || 'ungültige email adresse' ]"
+          type="email" />
       </v-col>
       <v-col cols="3" class="text-">
         <v-select
@@ -14,21 +45,26 @@
           :items="filterOpts" />
       </v-col>
       <v-col class="text-right px-0" cols="6">
-        <v-btn elevation="0" color="primary" @click="sendMatches">
+        <v-btn
+          :loading="isSending"
+          :disabled="isSending"
+          elevation="0"
+          color="primary"
+          @click="sendMatches">
           {{ searchTable.filter(r => r.candidateSelected > -1).length }} von {{searchTable.length}} Personen Abfragen
         </v-btn>
       </v-col>
-      <v-col cols="12" class="text-center">
+      <v-col cols="12" class="pr-0 mr-0 text-center">
         <query-progress :search-table="searchTable" />
       </v-col>
-    </v-row>
+    </v-form>
     <v-divider />
-    <v-row class="flex-shrink-0" style="position: relative; height: 80%">
+    <v-row class="flex-shrink-0" style="position: relative; height: 85%">
       <v-container class="fill-height pa-0">
         <v-row class="fill-height">
           <v-col style="position: relative" class="fill-height pa-0 px-3">
             <RecycleScroller
-              class="scroller fill-height v-list--nav"
+              class="scroller fill-height v-list--nav pl-0"
               :items="searchTableFiltered"
               key-field="id"
               :item-size="60">
@@ -42,7 +78,7 @@
               </template>
             </RecycleScroller>
           </v-col>
-          <v-col class="fill-height py-0" cols="5">
+          <v-col class="fill-height py-0 pl-0" cols="5">
             <v-card class="fill-height d-flex flex-column rounded-lg" scrollable rounded elevation="0">
               <v-card-title class="pb-0">
                 <v-subheader>Eingabe</v-subheader>
@@ -96,7 +132,7 @@ import ResearchPersonItem from './ResearchPersonItem.vue'
 import SearchPersonDetail from './SearchPersonDetail.vue'
 import _ from 'lodash'
 import * as lobid from '../service/lobid'
-import { saveAs } from 'file-saver'
+// import { saveAs } from 'file-saver'
 import { PersonField, PersonMatchable } from '@/types'
 import { postList } from '@/service/backend'
 import QueryProgress from './QueryProgress.vue'
@@ -113,6 +149,10 @@ export default class PersonQueryMultiple extends Vue {
   selectedPersonId = '1'
   searchResultPersonDebounced = _.debounce(this.searchResultPerson, 300)
   searchingLobidPerson = false
+  isFormValid = true
+  isSending = false
+  successId: string|null = null
+  host = window.location.host
   eMail = ''
 
   get filterOpts(): Array<{text: string, value: string, filter: (e: PersonMatchable) => boolean}> {
@@ -128,12 +168,12 @@ export default class PersonQueryMultiple extends Vue {
         filter: (e: PersonMatchable) => e.lobid.length === 0 && e.loaded === true
       },
       {
-        text: 'ambig',
+        text: 'mehrdeutig',
         value: 'ambiguous',
         filter: (e: PersonMatchable) => e.lobid.length > 1
       },
       {
-        text: 'disambiguiert',
+        text: 'gefunden',
         value: 'found',
         filter: (e: PersonMatchable) => e.lobid.length === 1 || e.candidateSelected > -1
       }
@@ -166,27 +206,31 @@ export default class PersonQueryMultiple extends Vue {
     this.searchTable[i].candidateSelected = lobidPersonIndex
   }
 
+  isNormalEmail(email: string): boolean {
+    return email.includes('@') && email.includes('.')
+  }
+
   async sendMatches(): Promise<void> {
-    const res = this.searchTable
-      .filter(p => p.candidateSelected > -1)
-      .map(p => {
-        return {
-          firstName: p.firstName as string,
-          lastName: p.lastName as string,
-          gnd: (p.lobid[p.candidateSelected] as any).gndIdentifier as string
-        }
-      })
-    // console.log(res)
-    await postList(res, this.eMail)
-    saveAs(new Blob([JSON.stringify({email: this.eMail, lemmas: res}, undefined, 4)]), 'research-list-matched.json')
+    if ((this.$refs.form as any).validate() === true) {
+      const res = this.searchTable
+        .filter(p => p.candidateSelected > -1)
+        .map(p => {
+          return {
+            firstName: p.firstName as string,
+            lastName: p.lastName as string,
+            gnd: (p.lobid[p.candidateSelected] as any).gndIdentifier as string
+          }
+        })
+      this.isSending = true
+      const s = await postList(res, this.eMail)
+      this.successId = s.success
+      this.isSending = false
+      // saveAs(new Blob([JSON.stringify({email: this.eMail, lemmas: res}, undefined, 4)]), 'research-list-matched.json')
+    }
   }
 
   findIndexById(id: string): number {
     return this.searchTableFiltered.findIndex(r => r.id === id)
-  }
-
-  mounted() {
-    
   }
 }
 </script>
