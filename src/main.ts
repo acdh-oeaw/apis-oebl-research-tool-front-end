28ffc28d29db673a8a4e7a40e7cd7dde081e548a
arnold-graf <arnold.graf@gmail.com>
2020-10-30 17:15:11 +0100

    install line clamp

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import lineClamp from 'vue-line-clamp'
 
 Vue.config.productionTip = false
+
+Vue.use(lineClamp)
 
 new Vue({
   router,
