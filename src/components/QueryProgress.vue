8f9e6ac8c4ce42d09d513cc80704105a6ce09803
arnold-graf <arnold.graf@gmail.com>
2020-11-13 17:01:36 +0100

    use filter type for progress

--- src/components/QueryProgress.vue
+++ src/components/QueryProgress.vue
@@ -1,47 +1,65 @@
 <template>
-  <div class="outer-bar text-left">
-    <div :title="progress.found + ' gefunden'" class="bar" :style="{width: progress.found / total * 100 + '%', background: '#4caf50'}">
-      {{ progress.found }} gefunden
-    </div>
-    <div :title="progress.ambiguous + ' mehrdeutig'" class="bar" :style="{width: progress.ambiguous / total * 100 + '%', background: '#f44236'}">
-      {{ progress.ambiguous }} mehrdeutig
-    </div>
-    <div :title="progress.notFound + ' nicht gefunden'" class="bar" :style="{width: progress.notFound / total * 100 + '%', background: '#ababab'}">
-      {{ progress.notFound }} nicht gefunden
+  <div :class="['outer-bar text-left', activeFilter.value !== 'all' && 'filter-selected']">
+    <div
+      v-for="filter in filtersNotAll"
+      :key="filter.value"
+      :title="filter.count + ' ' + filter.text"
+      :class="['bar', activeFilter.value === filter.value && 'active']"
+      @click="$emit('select-filter', filter)"
+      :style="{width: filter.count / total * 100 + '%', background: filter.color}">
+      {{ filter.count + ' ' + filter.text }}
     </div>
   </div>
 </template>
 <script lang="ts">
 
+import { Filter } from '@/types'
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 
 @Component
-export default class Progress extends Vue {
+export default class QueryProgress extends Vue {
 
-  @Prop() progress: { found: number, notFound: number, ambiguous: number }
+  @Prop() filters: Filter[]
+  @Prop() activeFilter: Filter
   @Prop() total: number
+
+  get filtersNotAll(): Filter[] {
+    return this.filters.filter(f => f.value !== 'all')
+  }
 
 }
 </script>
 <style lang="stylus" scoped>
 .outer-bar{
-  background: repeating-linear-gradient(45deg, #ccc, #ccc 10px, #a5a5a5 10px, #a5a5a5 20px)
-  border-radius 5px
+  background: repeating-linear-gradient(45deg, #dcdcdc, #dcdcdc 5px, transparent 5px, transparent 10px)
+  border-radius 12px
   overflow hidden
   position relative
-  height: 20px
+  height 20px
+}
+.outer-bar:hover .bar, .outer-bar.filter-selected .bar{
+  filter contrast(.5)
 }
 .bar{
+  text-shadow 1px 1px 4px rgba(0,0,0,0.5)
   border-right: 1px solid rgba(255,255,255,.15)
   text-overflow: ellipsis
   white-space: nowrap;
   padding: 0 0.5em;
-  transition .25s width
+  transition .25s width, .25s filter
   text-align center
   font-size small
   color white
   overflow hidden
   float left
   height: 100%
+  cursor pointer
+}
+.bar:hover, .bar.active {
+  filter contrast(1) !important
+}
+.bar:last-child{
+  border-top-right-radius: 12px
+  border-bottom-right-radius: 12px
 }
 </style>
