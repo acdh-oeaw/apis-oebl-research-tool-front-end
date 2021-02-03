0d6ddd54f85195f7048cf7367a345169d2a7784d
arnold-graf <arnold.graf@gmail.com>
2021-02-03 17:43:17 +0100

    formatters for keys and values in scrape view

--- src/views/LemmaManager/LemmaScrapeResult.vue
+++ src/views/LemmaManager/LemmaScrapeResult.vue
@@ -14,14 +14,16 @@
     <v-list-item
       dense
       v-for="(scrapeDataValue, key) in value" :key="key"
+      :href="maybeLinkItem(key, scrapeDataValue)"
+      target="_blank"
       v-if="!Array.isArray(scrapeDataValue)">
-      <v-list-item-content class="list-item-label">{{ key }}</v-list-item-content>
+      <v-list-item-content class="list-item-label">{{ formatKey(key) }}</v-list-item-content>
       <v-list-item-action-text
         :class="[
           'list-item-value',
           (typeof scrapeDataValue === 'string' && scrapeDataValue.length > 300) && 'longform-text'
         ]">
-        {{ scrapeDataValue }}
+        {{ formatValue(key, scrapeDataValue) }}
       </v-list-item-action-text>
     </v-list-item>
     <v-list-group
@@ -30,17 +32,22 @@
       v-if="Array.isArray(scrapeDataValue)"
       :class="[scrapeDataValue.length === 0 && 'list-disabled']">
       <template v-slot:activator>
-        <v-list-item-title>{{ key }}</v-list-item-title>
+        <v-list-item-title>{{ formatKey(key) }}</v-list-item-title>
         <v-list-item-action-text>{{ scrapeDataValue.length }}</v-list-item-action-text>
       </template>
-      <v-list-item dense v-for="(obvDataV, key) in scrapeDataValue" :key="key">
+      <v-list-item
+        dense
+        v-for="(subValue, key) in scrapeDataValue"
+        :href="maybeLinkItem(key, subValue)"
+        target="_blank"
+        :key="key">
         <v-list-item-content v-if="typeof key !== 'number'" class="list-item-label">{{ key }}</v-list-item-content>
         <v-list-item-action-text
           :class="[
             'list-item-value',
-            (typeof obvDataV === 'string' && obvDataV.length > 300) && 'longform-text'
+            (typeof subValue === 'string' && subValue.length > 300) && 'longform-text'
           ]">
-            {{ obvDataV }}
+            {{ formatValue(key, subValue) }}
         </v-list-item-action-text>
       </v-list-item>
     </v-list-group>
@@ -49,11 +56,47 @@
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { ServerResearchLemma } from '@/types/lemma'
+import _ from 'lodash'
+import { maybeParseDate, isValidHttpUrl } from '@/util'
+import formatDate from 'date-fns/esm/format'
+import de from 'date-fns/esm/locale/de'
 
 @Component
 export default class LemmaScrapeResult extends Vue {
+
   @Prop({ required: true }) value!: ServerResearchLemma['columns_scrape'][keyof ServerResearchLemma['columns_scrape']]
   @Prop({ required: true }) title!: string
+
+  keyNamesReadable: { [key: string]: string } = {
+    txt: ''
+  }
+
+  formatKey(s: string) {
+    if (this.keyNamesReadable[s] !== undefined) {
+      return ''
+    } else {
+      return _.startCase(s)
+    }
+  }
+
+  formatValue(key: string|number, value: any) {
+    const maybeDate = maybeParseDate(value)
+    if (maybeDate !== null) {
+      return formatDate(maybeDate, 'do MMM. yyyy', {locale: de })
+    } else {
+      return value
+    }
+  }
+
+  maybeLinkItem(key: string|number, value: any): string|undefined {
+    if (isValidHttpUrl(value)) {
+      return value
+    }
+    // if (key === 'loc') {
+    //   return
+    // }
+  }
+
 }
 </script>
 <style lang="stylus" scoped>
@@ -65,13 +108,13 @@ export default class LemmaScrapeResult extends Vue {
   overflow: visible
 
 .longform-text
-  text-align justify !important
   font-size 15px !important
+
 </style>
 <style lang="stylus">
 .scrape-result .v-list-item--active
   background var(--v-background-base)
   position sticky
   z-index 1
-  top: -16px
+  top 0
 </style>
