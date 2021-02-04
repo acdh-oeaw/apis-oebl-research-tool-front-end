7d8651ca1be38f2b2283a2cbf0fb84b5d2287fdf
arnold-graf <arnold.graf@gmail.com>
2021-02-04 17:07:59 +0100

    get editor by id

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -1,9 +1,11 @@
 import { WorkflowService, Editor } from '@/api'
+import _ from 'lodash'
 import request from './request'
 
 export default class EditorStore {
 
   private _editors: Editor[] = []
+  public editorsById: { [id: number]: Editor } = {}
 
   constructor() {
     this.loadEditors()
@@ -11,6 +13,7 @@ export default class EditorStore {
 
   async loadEditors() {
     this._editors = (await request(WorkflowService.workflowApiV1EditorsList, undefined)).results || []
+    this.editorsById = _.keyBy(this._editors, 'userId')
   }
 
   getById(id: number) {
