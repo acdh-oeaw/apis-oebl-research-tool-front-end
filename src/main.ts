1fc3fb368b487b9df324319bea9b1cab7be25e1a
arnold-graf <arnold.graf@gmail.com>
2021-02-10 16:45:35 +0100

    experiment with default props

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+// import { VTextarea } from 'vuetify/lib'
 
 Vue.config.productionTip = false
+
+// const irsTextField: Vue.Component = {
+//   extends: VTextarea,
+//   mixins: [
+//     {
+//       props: {
+//         flat: { default: true },
+//         solo: { default: true },
+//         dense: { default: true },
+//         autoGrow: { default: true },
+//         rows: { default: '1' },
+//         backgroundColor: 'background darken-2',
+//         hideDetails: { default: true }
+//       }
+//     }
+//   ]
+// }
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  // components: { irsTextField }
 }).$mount('#app')
