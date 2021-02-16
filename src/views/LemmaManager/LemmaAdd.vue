bf78f09bf724dfd656ce8eb620e0b0855f128396
arnold-graf <arnold.graf@gmail.com>
2021-02-16 16:33:56 +0100

    fix lemma add layout

--- src/views/LemmaManager/LemmaAdd.vue
+++ src/views/LemmaManager/LemmaAdd.vue
@@ -1,11 +1,26 @@
 <template>
   <v-card :color="color" rounded="lg" class="soft-shadow">
     <v-card-title>
-      Lemma anlegen
-    </v-card-title>
-    <v-card-text class="pa-0 flex-grow-1">
       <v-row no-gutters>
-        <v-col class="pa-5">
+        <v-col>
+          <v-btn @click="$emit('cancel')" class="rounded-lg px-4" color="background darken-1" elevation="0">
+            Abbrechen
+          </v-btn>
+        </v-col>
+        <v-col class="text-center">
+          Lemma anlegen
+        </v-col>
+        <v-col class="text-right">
+          <v-btn @click="addLemma" class="rounded-lg px-4" color="primary" elevation="0">
+            Lemma hinzufügen
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-title>
+    <v-divider :style="{ opacity: showDivider ? 1 : 0, transition: '.5s opacity' }" />
+    <v-card-text class="pa-0 flex-grow-1 overflow-y-hidden fill-height">
+      <v-row no-gutters class="fill-height">
+        <v-col @scroll.passive="onScroll" class="pa-5 fill-height overflow-y-auto">
           <h4 class="pb-4 px-1">Basisdaten</h4>
           <text-field
             autofocus
@@ -39,75 +54,66 @@
             :label="column.name"
           />
         </v-col>
-        <v-col cols="5" class="background darken-1 pa-4">
-          <v-window class="pt-1" reverse :value="viewLemmaDetail === null ? 0 : 1">
-            <v-window-item :value="0">
-              <v-btn-toggle :value="window" borderless style="width: 50%" class="mb-4">
-                <v-btn rounded small @click="window = 0" block elevation="0" color="background darken-2">
-                  ÖBL <v-badge offset-x="-3" color="secondary" v-if="filteredList.length > 0" :content="filteredList.length.toString()" />
-                </v-btn>
-                <v-btn rounded small @click="window = 1" block elevation="0" color="background darken-2">
-                  Lobid <v-badge offset-x="-3" color="secondary" v-if="possibleGnds.length > 0" :content="possibleGnds.length.toString()" />
-                </v-btn>
-              </v-btn-toggle>
-              <v-window reverse :value="window">
-                <v-window-item style="position: relative">
-                  <v-overlay v-if="filteredList.length === 0" absolute>Keine Ähnlichen Lemmata gefunden</v-overlay>
-                  <v-list
-                    dense
-                    style="overflow: scroll; height: 400px"
-                    color="transparent">
-                    <v-list-item
-                      class="rounded-lg"
-                      v-for="lemma in filteredList"
-                      :key="lemma.id"
+        <v-col cols="5" class="background darken-1 pa-4 fill-height overflow-y-hidden">
+          <v-window class="pt-1 fill-height" reverse :value="viewLemmaDetail === null ? 0 : 1">
+            <v-window-item class="fill-height" :value="0">
+              <div class="d-flex flex-column fill-height">
+                <v-btn-toggle :value="window" borderless style="width: 50%" class="mb-4">
+                  <v-btn rounded small @click="window = 0" block elevation="0" color="background darken-2">
+                    ÖBL <v-badge offset-x="-3" color="secondary" v-if="filteredList.length > 0" :content="filteredList.length.toString()" />
+                  </v-btn>
+                  <v-btn rounded small @click="window = 1" block elevation="0" color="background darken-2">
+                    GND-Suche <v-badge offset-x="-3" color="secondary" v-if="possibleGnds.length > 0" :content="possibleGnds.length.toString()" />
+                  </v-btn>
+                </v-btn-toggle>
+                <v-window class="flex-grow-1 fill-height" reverse :value="window">
+                  <v-window-item class="fill-height">
+                    <v-overlay v-if="filteredList.length === 0" absolute>Keine ähnlichen Lemmata gefunden</v-overlay>
+                    <v-list
                       dense
-                      @click="viewLemmaDetail = lemma">
-                      <v-list-item-avatar width="15">
-                        <v-icon small v-if="lemma.selected" color="primary">mdi-star</v-icon>
-                        <v-icon small v-else>mdi-star-outline</v-icon>
-                      </v-list-item-avatar>
-                      <v-list-item-content>
-                        <v-list-item-title>
-                          {{ lemma.firstName }} {{ lemma.lastName }}
-                        </v-list-item-title>
-                        <v-list-item-subtitle>
-                          {{ lemma.birthYear }} - {{ lemma.deathYear }}
-                        </v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </v-list>
-                </v-window-item>
-                <v-window-item class="position: relative">
-                  <lobid-preview-card
-                    style="overflow: scroll; height: 400px"
-                    @input="person.gnd = $event"
-                    :gnd="possibleGnds" />
-                  <v-overlay v-if="possibleGnds.length === 0" absolute>Keine GNDs bei Lobid gefunden</v-overlay>
-                </v-window-item>
-              </v-window>
+                      class="overflow-y-auto fill-height"
+                      color="transparent">
+                      <v-list-item
+                        class="rounded-lg"
+                        v-for="lemma in filteredList"
+                        :key="lemma.id"
+                        dense
+                        @click="viewLemmaDetail = lemma">
+                        <v-list-item-avatar width="15">
+                          <span v-if="lemma.selected === true" style="color: var(--v-primary-base)">★</span>
+                          <span v-else style="opacity: .5">☆</span>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                          <v-list-item-title>
+                            {{ lemma.firstName }} {{ lemma.lastName }}
+                          </v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ lemma.birthYear }} - {{ lemma.deathYear }}
+                          </v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list>
+                  </v-window-item>
+                  <v-window-item class="fill-height">
+                    <lobid-preview-card
+                      class="fill-height overflow-y-auto"
+                      @input="person.gnd = $event"
+                      :gnd="possibleGnds" />
+                    <v-overlay v-if="possibleGnds.length === 0" absolute>Keine GNDs bei Lobid gefunden</v-overlay>
+                  </v-window-item>
+                </v-window>
+              </div>
             </v-window-item>
-            <v-window-item :value="1">
-              <v-btn @click="viewLemmaDetail = null" text><v-icon left>mdi-chevron-left</v-icon> Ergebnisse</v-btn>
-              <lemma-detail style="overflow: scroll; height: 450px" v-if="viewLemmaDetail !== null" :value="viewLemmaDetail" />
+            <v-window-item class="fill-height" :value="1">
+              <div class="d-flex flex-column fill-height">
+                <v-btn @click="viewLemmaDetail = null" text><v-icon left>mdi-chevron-left</v-icon> Ergebnisse</v-btn>
+                <lemma-detail class="flex-grow-1 fill-height background" v-if="viewLemmaDetail !== null" :value="viewLemmaDetail" />
+              </div>
             </v-window-item>
           </v-window>
         </v-col>
       </v-row>
-      <!-- alle default fields für ein lemma -->
-      <!-- automatische GND suche via LOBID -->
-      <!-- automatisch alle bestehenden lemmata mit Furry/Levensthein durchsuchen und als vorschläge ausgeben -->
     </v-card-text>
-    <!-- <v-divider /> -->
-    <v-card-actions class="pa-4 background lighten-2">
-      <v-btn @click="$emit('cancel')" class="rounded-lg px-4" color="background lighten-1" elevation="0">
-        Abbrechen
-      </v-btn>
-      <v-spacer />
-      <v-btn @click="addLemma" class="rounded-lg px-4" color="primary" elevation="0">
-        Lemma hinzufügen
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 <script lang="ts">
@@ -131,7 +137,9 @@ import _ from 'lodash'
 export default class LemmaAdd extends Vue {
 
   @Prop({ default: undefined }) color!: string|undefined
+
   window = 0
+  showDivider = false
   store = store
   possibleGnds: string[] = []
   viewLemmaDetail: LemmaRow|null = null
@@ -141,6 +149,14 @@ export default class LemmaAdd extends Vue {
     dateOfBirth: null,
     dateOfDeath: null,
     gnd: []
+  }
+
+  onScroll(e: MouseEvent) {
+    if (e.target instanceof HTMLElement && e.target.scrollTop > 0) {
+      this.showDivider = true
+    } else {
+      this.showDivider = false
+    }
   }
 
   person: ImportablePerson = this.emptyPerson
