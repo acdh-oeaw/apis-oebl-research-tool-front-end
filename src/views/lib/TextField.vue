0cc0c670ba4557e842773092f22846855c35936c
arnold-graf <arnold.graf@gmail.com>
2021-02-18 17:27:57 +0100

    emit only valid values from textfield

--- src/views/lib/TextField.vue
+++ src/views/lib/TextField.vue
@@ -10,14 +10,13 @@
         {{ $attrs.label }}
       </div>
       <div style="position: relative" class="fill-width">
-        <div class="text-body-2 fill-height fill-width fake-textarea mt-2 mr-2" v-text="internalValue" />
+        <div class="text-body-2 fill-height fill-width fake-textarea mt-2 mr-2" v-text="localValue" />
         <textarea
           class="fill-height fill-width mt-2 mr-2 text-body-2 "
           style="position: absolute; top: 0; right: 0; bottom: 0; left: 0"
           @keydown="onKeyDown"
           @input="onInput"
-          v-bind="$attrs"
-          :value="internalValue" />
+          :value="localValue" />
       </div>
     </div>
     <div class="text-center caption hint" v-if="msg !== null" v-text="msg" />
@@ -32,40 +31,49 @@ export default class TextField extends Vue {
 
   @Prop({ default: () => [] }) rules!: Array<(e: string|null) => string|false>
   @Prop({ default: false }) allowNewLine!: boolean
+  @Prop({ default: '' }) value!: string
+  @Prop({ default: false }) required!: boolean
 
-  internalValue: string|null = this.$attrs.value || ''
   msg: string|null = null
+
+  localValue = this.value
+
+  @Watch('value')
+  onChangeValue() {
+    this.localValue = this.value
+  }
 
   selectAll() {
     (this.$el.querySelector('textarea') as HTMLTextAreaElement).select()
   }
 
-  checkValid(e: string|null) {
+  checkValid(e: string|null): boolean {
     if (e != null && e !== '') {
       const r = this.rules.find((r) => {
         return r(e) !== false
       })
       if (r) {
         this.msg = r(e) || null
+        return false
       } else {
         this.msg = null
+        return true
       }
+    } else if (this.required) {
+      this.msg = 'Dieses Feld darf nicht leer sein.'
+      return false
     } else {
       this.msg = null
+      return true
     }
   }
 
-  emitVal(v: string) {
-    this.$emit('input', v)
-  }
-
-  debouncedEmitVal = _.debounce(this.emitVal, 300)
-
   onInput(e: InputEvent) {
     if (e.target instanceof HTMLTextAreaElement) {
-      this.internalValue = e.target.value
-      this.checkValid(e.target.value)
-      this.debouncedEmitVal(e.target.value)
+      this.localValue = e.target.value
+      if (this.checkValid(e.target.value)) {
+        this.$emit('input', e.target.value)
+      }
     }
   }
 
