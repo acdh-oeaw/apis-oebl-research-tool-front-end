f0f35bc9f6bf1b5a381c447c6648925b216143fb
arnold-graf <arnold.graf@gmail.com>
2021-02-19 17:08:29 +0100

    rm comments

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-// import { VTextarea } from 'vuetify/lib'
 
 Vue.config.productionTip = false
-Vue.config.performance = true
-// const irsTextField: Vue.Component = {
-//   extends: VTextarea,
-//   mixins: [
-//     {
-//       props: {
-//         flat: { default: true },
-//         solo: { default: true },
-//         dense: { default: true },
-//         autoGrow: { default: true },
-//         rows: { default: '1' },
-//         backgroundColor: 'background darken-2',
-//         hideDetails: { default: true }
-//       }
-//     }
-//   ]
-// }
+// Vue.config.performance = true
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App),
-  // components: { irsTextField }
+  render: h => h(App)
 }).$mount('#app')
