f0b05ee0b993fbd327d0f839641f13980bdb5109
arnold-graf <arnold.graf@gmail.com>
2021-04-23 15:00:14 +0200

    perf config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
-// Vue.config.performance = true
+Vue.config.performance = true
 
 new Vue({
   router,
