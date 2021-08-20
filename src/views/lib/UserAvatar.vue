9b13a64cc295bff5c79df35370e81065994cbec0
arnold-graf <arnold.graf@gmail.com>
2021-08-20 15:16:55 +0200

    avatar style

--- src/views/lib/UserAvatar.vue
+++ src/views/lib/UserAvatar.vue
@@ -1,7 +1,10 @@
 <template>
   <v-avatar
-    class="text-body-2"
-    :color="value !== null ? 'background darken-1' : 'background lighten-1'"
+    :class="[
+      'text-body-2',
+      value === null ? 'text--secondary font-weight-light' : 'text-primary',
+    ]"
+    :color="value !== null ? 'background darken-1' : 'transparent'"
     min-width="30"
     height="30"
     width="30">
