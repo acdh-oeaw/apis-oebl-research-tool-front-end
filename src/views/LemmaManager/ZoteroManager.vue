83e6af6a1f9f36d73d14652f8614ec0e36c22682
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-02-16 16:23:30 +0100

    Simple Dummy Zotero Citations View
    Done:
    
    - Writing ZoteroManager component.
    - Using it in LemmaDetail.
    - Injecting ZoteroLemmaServerConnector there (which is currently only a dummy).
    
    TODO:
    
    - Build a cache for zotero
    - Implement ZoteroLemmaServerConnector functionallity
    - Fix styling of ZoteroLemmaServerConnector:
      - Background color is wrong
      - Links appear to have wrong color, are browser-build in ul-style and may have a wrong font
    
    Tags:
    Issue #17
    [skip ci]

---
+++ src/views/LemmaManager/ZoteroManager.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-expansion-panel>
+        <v-expansion-panel-header>{{ title }} ({{ zoteroItems.length }})</v-expansion-panel-header>
+        <v-expansion-panel-content>
+            <ul class="zotero-citation-list">
+                <li
+                    v-for="(zoteroView, key) in zoteroItemsView"
+                    :key="key"
+                >
+                    <a :href="zoteroView.url" target="_blank">{{ zoteroView.title }}</a>
+                </li>
+            </ul>
+        </v-expansion-panel-content>
+    </v-expansion-panel>
+</template>
+
+<script lang="ts">
+
+import {Vue, Component, Prop} from 'vue-property-decorator';
+
+
+interface ZoteroView {
+    title: string,
+    url: string,
+}
+
+import { ZoteroLemmaServerConnector, ZoteroItem } from '@/service/zotero';
+
+/**
+ * Manage Zotero Items from and about a lemma (https://gitlab.com/acdh-oeaw/oebl/oebl-research-tool-front-end/-/issues/17):
+ * 
+ * - List items
+ * - … more to come
+ */
+@Component
+export default class ZoteroManager extends Vue {
+
+    @Prop() ZoteroLemmaServerConnector!: ZoteroLemmaServerConnector;
+    @Prop({ default: 'Zotero Literatur' }) title!: string;
+
+    zoteroItems: Array<ZoteroItem> = [];
+
+    created() {
+        this.ZoteroLemmaServerConnector
+            .get().
+            then(
+                (zoteroItems: ZoteroItem[]) => this.zoteroItems = zoteroItems
+            )
+        ;
+    }
+
+    get zoteroItemsView(): Array<ZoteroView> {
+        return this.zoteroItems.map(
+            (zoteroItem: ZoteroItem): ZoteroView => {
+                return {
+                    title: zoteroItem.data.title,
+                    url: zoteroItem.key, // TODO: This should be a valid url
+                }
+            }
+        );
+    } 
+
+
+}
+
+</script>
+
