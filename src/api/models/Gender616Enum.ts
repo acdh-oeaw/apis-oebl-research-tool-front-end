c794755acbd7fd5d876601a955a26b7b38647b0b
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-02-21 10:28:53 +0100

    Auto Generated New Models from backend
    Related to the backend commits:
    
    - https://gitlab.com/acdh-oeaw/oebl/oebl-irs-devops/-/commit/598268cddec8728a7bf0370571ead17aa320c09a
    - https://gitlab.com/acdh-oeaw/oebl/oebl-irs-devops/-/commit/03447a0c57a4aece2c2026516a45a1fb8aafd7bd
    
    Tags:
    
    [skip ci]
    Issue #19

---
+++ src/api/models/Gender616Enum.ts
@@ -0,0 +1,9 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export enum Gender616Enum {
+    FEMALE = 'female',
+    MALE = 'male',
+    THIRD_GENDER = 'third gender',
+}
