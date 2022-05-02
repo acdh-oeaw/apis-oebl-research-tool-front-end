8b480a1d5447a39510ef6eef5b0431ec788b186a
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-05-02 11:11:55 +0200

    Auto Format Code [skip ci]

--- src/views/LemmaManager/LemmaPrintView.vue
+++ src/views/LemmaManager/LemmaPrintView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lemma-print-view-container">
     <section v-if="lemma !== null" class="lemma-print-view">
-      <h1 >
+      <h1>
         <span class="last-name-title">{{ lemma.lastName }}</span>
         <span v-if="lemma.firstName" class="first-name-title">{{
           lemma.firstName
@@ -14,83 +14,90 @@
             <h3>Name</h3>
             <div class="primary-name">
               <div class="field last-name">
-                <div class="fieldname">{{labels.lastName.de}}</div>
+                <div class="fieldname">{{ labels.lastName.de }}</div>
                 <div class="fieldvalue">{{ lemma.lastName }}</div>
               </div>
               <div class="field first-name">
-                <div class="fieldname">{{labels.firstName.de}}</div>
+                <div class="fieldname">{{ labels.firstName.de }}</div>
                 <div class="fieldvalue">{{ lemma.firstName }}</div>
               </div>
             </div>
             <div class="alternative-names">
-              <h3>{{labels.alternativeNames.de}}</h3>
-              <div 
+              <h3>{{ labels.alternativeNames.de }}</h3>
+              <div
                 v-for="(alternativeName, index) in lemma.alternativeNames"
                 :key="`alternativeNameNo_${index}`"
-                >
-                  <div class="field alternative-last-name">
-                    <div class="fieldname">{{labels.lastName.de}}</div>
-                    <div class="fieldvalue">{{ alternativeName.lastName }}</div>
-                  </div>
-                  <div class="field alternative-first-name">
-                    <div class="fieldname">{{labels.firstName.de}}</div>
-                    <div class="fieldvalue">{{ alternativeName.firstName }}</div>
-                  </div>
+              >
+                <div class="field alternative-last-name">
+                  <div class="fieldname">{{ labels.lastName.de }}</div>
+                  <div class="fieldvalue">{{ alternativeName.lastName }}</div>
+                </div>
+                <div class="field alternative-first-name">
+                  <div class="fieldname">{{ labels.firstName.de }}</div>
+                  <div class="fieldvalue">{{ alternativeName.firstName }}</div>
+                </div>
               </div>
             </div>
             <div class="field gender">
-              <div class="fieldname">{{labels.gender.de}}</div>
+              <div class="fieldname">{{ labels.gender.de }}</div>
               <div class="fieldvalue">{{ lemma.gender }}</div>
             </div>
             <div class="field dateOfBirth">
-              <div class="fieldname">{{labels.dateOfBirth.de}}</div>
+              <div class="fieldname">{{ labels.dateOfBirth.de }}</div>
               <div class="fieldvalue">{{ lemma.dateOfBirth }}</div>
             </div>
             <div class="field dateOfDeath">
-              <div class="fieldname">{{labels.dateOfDeath.de}}</div>
+              <div class="fieldname">{{ labels.dateOfDeath.de }}</div>
               <div class="fieldvalue">{{ lemma.dateOfDeath }}</div>
             </div>
             <div class="field kinship">
-              <div class="fieldname">{{labels.kinship.de}}</div>
+              <div class="fieldname">{{ labels.kinship.de }}</div>
               <div class="fieldvalue">{{ lemma.kinship }}</div>
             </div>
             <div class="field religion">
-              <div class="fieldname">{{labels.religion.de}}</div>
+              <div class="fieldname">{{ labels.religion.de }}</div>
               <div class="fieldvalue">{{ lemma.religion }}</div>
             </div>
             <div class="field professionDetail">
-              <div class="fieldname">{{labels.professionDetail.de}}</div>
+              <div class="fieldname">{{ labels.professionDetail.de }}</div>
               <div class="fieldvalue">{{ lemma.professionDetail }}</div>
             </div>
             <div class="field professionGroup">
-              <div class="fieldname">{{labels.professionGroup.de}}</div>
-              <div class="fieldvalue">{{ lemma.professionGroup ? lemma.professionGroup.name : '' }}</div>
+              <div class="fieldname">{{ labels.professionGroup.de }}</div>
+              <div class="fieldvalue">
+                {{ lemma.professionGroup ? lemma.professionGroup.name : "" }}
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section class="literature">
-        <h2>{{labels.secondaryLiterature.de}}</h2>
+        <h2>{{ labels.secondaryLiterature.de }}</h2>
         <div class="field zoteroKeysBy">
-              <div class="fieldname">{{labels.zoteroKeysBy.de}}</div>
-              <div class="fieldvalue">
-                <span 
-                  v-for="(zoteroView, index) in zoteroCitationsBy"
-                  :key="`zoteroCitationBy_${index}`"
-                  class="zotero-citation"
-                  >{{zoteroView.citation}}</span>
-              </div>
-            </div>
-            <div class="field zoteroCitationsAbout">
-              <div class="fieldname">{{labels.zoteroKeysAbout.de}}</div>
-              <div class="fieldvalue">
-                <span 
-                  v-for="(zoteroView, index) in zoteroCitationsAbout"
-                  :key="`zoteroCitationAbout_${index}`"
-                  class="zotero-citation"
-                  >{{zoteroView.citation}}</span>
-              </div>
-            </div>
+          <div class="fieldname">{{ labels.zoteroKeysBy.de }}</div>
+          <div class="fieldvalue">
+            <span
+              v-for="(zoteroView, index) in zoteroCitationsBy"
+              :key="`zoteroCitationBy_${index}`"
+              class="zotero-citation"
+              >{{ zoteroView.citation }}</span
+            >
+          </div>
+        </div>
+        <div class="field zoteroCitationsAbout">
+          <div class="fieldname">{{ labels.zoteroKeysAbout.de }}</div>
+          <div class="fieldvalue">
+            <span
+              v-for="(zoteroView, index) in zoteroCitationsAbout"
+              :key="`zoteroCitationAbout_${index}`"
+              class="zotero-citation"
+              >{{ zoteroView.citation }}</span
+            >
+          </div>
+        </div>
+      </section>
+      <section class="external-resources">
+        <h2>Externe Ressourcen</h2>
       </section>
     </section>
   </div>
@@ -102,19 +109,23 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 import { LemmaRow } from "@/types/lemma";
 import { LemmaDatabase, unserializeLemmaRow } from "@/store/lemma";
 import { lemmaRowTranslations } from "@/util/labels";
-import { convertZoteroItemToView, ZoteroLemmaManagmentController } from "@/service/zotero";
+import {
+  convertZoteroItemToView,
+  ZoteroLemmaManagmentController,
+} from "@/service/zotero";
 import { ZoteroView } from "@/types/zotero";
 
 const db = new LemmaDatabase();
-const zoteroBy: ZoteroLemmaManagmentController = new ZoteroLemmaManagmentController();
-const zoteroAbout: ZoteroLemmaManagmentController = new ZoteroLemmaManagmentController();
-
+const zoteroBy: ZoteroLemmaManagmentController =
+  new ZoteroLemmaManagmentController();
+const zoteroAbout: ZoteroLemmaManagmentController =
+  new ZoteroLemmaManagmentController();
 
 @Component
 export default class LemmaPrintView extends Vue {
   @Prop() lemmaId!: number;
   lemma: null | LemmaRow = null;
-  labels = lemmaRowTranslations
+  labels = lemmaRowTranslations;
 
   zoteroCitationsBy: ZoteroView[] = [];
   zoteroCitationsAbout: ZoteroView[] = [];
@@ -130,10 +141,24 @@ export default class LemmaPrintView extends Vue {
           );
           throw new Error(`Could not find Lemma with id <${this.lemmaId}>`);
         }
-        console.debug({serializedLemmaRow})
+        console.debug({ serializedLemmaRow });
         this.lemma = unserializeLemmaRow(serializedLemmaRow);
-        zoteroBy.load(this.lemma.zoteroKeysBy).then(z => this.zoteroCitationsBy = z.zoteroItems.map(convertZoteroItemToView));
-        zoteroAbout.load(this.lemma.zoteroKeysAbout).then(z => this.zoteroCitationsAbout = z.zoteroItems.map(convertZoteroItemToView));
+        zoteroBy
+          .load(this.lemma.zoteroKeysBy)
+          .then(
+            (z) =>
+              (this.zoteroCitationsBy = z.zoteroItems.map(
+                convertZoteroItemToView
+              ))
+          );
+        zoteroAbout
+          .load(this.lemma.zoteroKeysAbout)
+          .then(
+            (z) =>
+              (this.zoteroCitationsAbout = z.zoteroItems.map(
+                convertZoteroItemToView
+              ))
+          );
       })
       .catch((error) => {
         window.alert("Die Datenbank konnt nicht geladen werden.");
@@ -145,8 +170,6 @@ export default class LemmaPrintView extends Vue {
         throw new Error();
       });
   }
-
-    
 }
 </script>
 <style scoped>
@@ -155,7 +178,7 @@ export default class LemmaPrintView extends Vue {
 }
 
 .left-align {
-  margin-left: 1em;;
+  margin-left: 1em;
 }
 
 .fieldname {
@@ -163,9 +186,6 @@ export default class LemmaPrintView extends Vue {
 }
 
 .fieldname::after {
-  content: ':';
+  content: ":";
 }
-
-
-
 </style>
