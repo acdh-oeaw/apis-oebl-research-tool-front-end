a9e1eea68e61f6beca8bfb83e0d4285a6c68f305
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-05-03 11:10:17 +0200

    Styling For Print And Additional Styling Props For Child Components
    Changes to be committed:
    - modified:   LemmaPrintView.vue: 	Adding grid, font- and color style
    - modified:   LemmaScrapeResult.vue: 	Adding defaultExpand prop for printing view
    - modified:   LobidPreviewCard.vue:	Adding showFullLink prop for printing view
    
    Tags: Issue #56, [skip ci]

--- src/views/LemmaManager/LemmaPrintView.vue
+++ src/views/LemmaManager/LemmaPrintView.vue
@@ -1,108 +1,161 @@
 <template>
-  <div class="lemma-print-view-container">
+  <v-container class="lemma-print-view-container ">
     <section v-if="lemma !== null" class="lemma-print-view">
-      <h1>
-        <span class="last-name-title">{{ lemma.lastName }}</span>
-        <span v-if="lemma.firstName" class="first-name-title">{{
-          lemma.firstName
-        }}</span>
-      </h1>
+      <v-row>
+        <v-col>
+            <h1>
+            <span class="last-name-title">{{ lemma.lastName }}</span>
+            <span v-if="lemma.firstName" class="first-name-title">{{
+              lemma.firstName
+            }}</span>
+          </h1>
+        </v-col>
+      </v-row>
       <section class="main-data">
-        <h2>Basisinformationen</h2>
+        <v-row>
+          <v-col>
+            <h2>Basisinformationen</h2>
+          </v-col>
+        </v-row>
         <div class="main-data">
           <div class="name">
-            <h3>Name</h3>
-            <div class="primary-name">
-              <div class="field last-name">
-                <div class="fieldname">{{ labels.lastName.de }}</div>
-                <div class="fieldvalue">{{ lemma.lastName }}</div>
-              </div>
-              <div class="field first-name">
-                <div class="fieldname">{{ labels.firstName.de }}</div>
-                <div class="fieldvalue">{{ lemma.firstName }}</div>
-              </div>
-            </div>
-            <div class="alternative-names">
-              <h3>{{ labels.alternativeNames.de }}</h3>
-              <div
-                v-for="(alternativeName, index) in lemma.alternativeNames"
-                :key="`alternativeNameNo_${index}`"
-              >
-                <div class="field alternative-last-name">
-                  <div class="fieldname">{{ labels.lastName.de }}</div>
-                  <div class="fieldvalue">{{ alternativeName.lastName }}</div>
+            <v-row>
+              <v-col>
+                <h3>Name</h3>
+              </v-col>
+            </v-row>
+            <v-row class="primary-name" justify="start">
+              <v-col class="field last-name" cols="3">
+                <span class="fieldname">{{ labels.lastName.de }}</span>
+                <span class="fieldvalue">{{ lemma.lastName }}</span>
+              </v-col>
+              <v-col class="field first-name" cols="3">
+                <span class="fieldname">{{ labels.firstName.de }}</span>
+                <span class="fieldvalue">{{ lemma.firstName }}</span>
+              </v-col>
+            </v-row>
+            <v-row class="alternative-names" justify="start">
+              <v-col cols="3"><div class="fieldname">{{ labels.alternativeNames.de }}</div></v-col>
+              <v-col cols="9" class="alternative-names-wrapper-column">
+                 <!-- keep right of fieldname, even when they have more than one line. Seems overly complicated, but I do not know, how to do it better -->
+                <v-container class="alternative-names-wrapper-container">
+                  <v-row class="alternative-names-wrapper-row">
+                    <div
+                      v-for="(alternativeName, index) in lemma.alternativeNames"
+                      :key="`alternativeNameNo_${index}`"
+                      class="field alternative-name"
+                    >
+                      <v-col cols="1" class="field alternative-last-name">
+                        <div class="fieldname">{{ labels.lastName.de }}</div>
+                        <div class="fieldvalue">{{ alternativeName.lastName }}</div>
+                      </v-col>
+                      <v-col cols="1" class="field alternative-first-name">
+                        <div class="fieldname">{{ labels.firstName.de }}</div>
+                        <div class="fieldvalue">{{ alternativeName.firstName }}</div>
+                      </v-col>
+                    </div>
+                  </v-row>
+                </v-container>
+              </v-col>
+            </v-row>
+            <v-row class="demographic-row-1" justify="start">
+              <v-col cols="3">
+                <div class="field gender">
+                  <span class="fieldname">{{ labels.gender.de }}</span>
+                  <span class="fieldvalue">{{ lemma.gender }}</span>
                 </div>
-                <div class="field alternative-first-name">
-                  <div class="fieldname">{{ labels.firstName.de }}</div>
-                  <div class="fieldvalue">{{ alternativeName.firstName }}</div>
+              </v-col>
+              <v-col cols="4">
+                <div class="field dateOfBirth">
+                  <span class="fieldname">{{ labels.dateOfBirth.de }}</span>
+                  <span class="fieldvalue">{{ lemma.dateOfBirth }}</span>
                 </div>
+              </v-col>
+              <v-col cols="4">
+                <div class="field dateOfDeath">
+                  <span class="fieldname">{{ labels.dateOfDeath.de }}</span>
+                  <span class="fieldvalue">{{ lemma.dateOfDeath }}</span>
+                </div>
+              </v-col>
+            </v-row>
+            <v-row class="demographic-row-2" justify="start">
+              <v-col cols="3">
+                <div class="field professionGroup">
+                  <span class="fieldname">{{ labels.professionGroup.de }}</span>
+                  <span class="fieldvalue">
+                    {{ lemma.professionGroup ? lemma.professionGroup.name : "" }}
+                  </span>
               </div>
-            </div>
-            <div class="field gender">
-              <div class="fieldname">{{ labels.gender.de }}</div>
-              <div class="fieldvalue">{{ lemma.gender }}</div>
-            </div>
-            <div class="field dateOfBirth">
-              <div class="fieldname">{{ labels.dateOfBirth.de }}</div>
-              <div class="fieldvalue">{{ lemma.dateOfBirth }}</div>
-            </div>
-            <div class="field dateOfDeath">
-              <div class="fieldname">{{ labels.dateOfDeath.de }}</div>
-              <div class="fieldvalue">{{ lemma.dateOfDeath }}</div>
-            </div>
-            <div class="field kinship">
-              <div class="fieldname">{{ labels.kinship.de }}</div>
-              <div class="fieldvalue">{{ lemma.kinship }}</div>
-            </div>
-            <div class="field religion">
-              <div class="fieldname">{{ labels.religion.de }}</div>
-              <div class="fieldvalue">{{ lemma.religion }}</div>
-            </div>
-            <div class="field professionDetail">
-              <div class="fieldname">{{ labels.professionDetail.de }}</div>
-              <div class="fieldvalue">{{ lemma.professionDetail }}</div>
-            </div>
-            <div class="field professionGroup">
-              <div class="fieldname">{{ labels.professionGroup.de }}</div>
-              <div class="fieldvalue">
-                {{ lemma.professionGroup ? lemma.professionGroup.name : "" }}
-              </div>
-            </div>
+              </v-col>
+              <v-col cols="4">
+                <div class="field professionDetail">
+                  <span class="fieldname">{{ labels.professionDetail.de }}</span>
+                  <span class="fieldvalue">{{ lemma.professionDetail }}</span>
+                </div>
+              </v-col>
+              <v-col cols="4">
+                <div class="field religion">
+                  <span class="fieldname">{{ labels.religion.de }}</span>
+                  <span class="fieldvalue">{{ lemma.religion }}</span>
+                </div>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <div class="field kinship">
+                  <div class="fieldname">{{ labels.kinship.de }}</div>
+                  <div class="fieldvalue">{{ lemma.kinship }}</div>
+                </div>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <div class="field bioNote">
+                  <div class="fieldname">{{ labels.bioNote.de }}</div>
+                  <div class="fieldvalue">{{ lemma.bioNote }}</div>
+                </div>
+              </v-col>
+            </v-row>
           </div>
         </div>
       </section>
       <section class="literature">
-        <h2>{{ labels.secondaryLiterature.de }}</h2>
+        <v-row><v-col><h2>{{ labels.secondaryLiterature.de }}</h2></v-col></v-row>
         <div class="field zoteroKeysBy">
-          <div class="fieldname">{{ labels.zoteroKeysBy.de }}</div>
+          <v-row><v-col><div class="fieldname">"{{ labels.zoteroKeysBy.de }}" ({{zoteroCitationsBy.length}})</div></v-col></v-row>
           <div class="fieldvalue">
-            <span
+            <v-row
               v-for="(zoteroView, index) in zoteroCitationsBy"
               :key="`zoteroCitationBy_${index}`"
               class="zotero-citation"
-              >{{ zoteroView.citation }}</span
-            >
+              ><v-col>{{ zoteroView.citation }}</v-col></v-row>
           </div>
         </div>
         <div class="field zoteroCitationsAbout">
-          <div class="fieldname">{{ labels.zoteroKeysAbout.de }}</div>
+          <v-row><v-col><div class="fieldname">"{{ labels.zoteroKeysAbout.de }}" ({{zoteroCitationsAbout.length}})</div></v-col></v-row>
           <div class="fieldvalue">
-            <span
+            <v-row
               v-for="(zoteroView, index) in zoteroCitationsAbout"
               :key="`zoteroCitationAbout_${index}`"
               class="zotero-citation"
-              >{{ zoteroView.citation }}</span
+              ><v-col>{{ zoteroView.citation }}</v-col></v-row
             >
           </div>
         </div>
       </section>
       <section class="external-resources">
-        <h2>Externe Ressourcen</h2>
-        <lobid-preview-card
-          v-if="lemma.gnd.length > 0"
-          :limit="1"
-          :gnd="lemma.gnd" 
-        />
+        <v-row><v-col><h2>Externe Ressourcen</h2></v-col></v-row>
+        <v-row><v-col><h3>GND</h3></v-col></v-row>
+        <v-row>
+          <v-col>
+            <lobid-preview-card
+              v-if="lemma.gnd.length > 0"
+              :limit="1"
+              :gnd="lemma.gnd" 
+              :showFullLink="true"
+            />
+          </v-col>
+        </v-row>
         <div 
           v-if="lemma.columns_scrape"
           class="scrape-data">
@@ -110,12 +163,13 @@
             v-for="(source, sourceName) in lemma.columns_scrape"
             :key="sourceName"
             :value="source"
-            :title="sourceName" 
+            :title="sourceName"
+            :defaultExpand="true"
           />
         </div>
       </section>
     </section>
-  </div>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -206,9 +260,54 @@ export default class LemmaPrintView extends Vue {
 
 .fieldname {
   font-weight: 600;
+  margin-right: 1em;
 }
 
 .fieldname::after {
   content: ":";
 }
+
+.alternative-names h3::after {
+  content: ':';
+}
+
+.alternative-names .field .fieldname {
+  display: none;
+}
+
+.alternative-names .field, .alternative-names .fieldvalue {
+  display: inline;
+}
+
+
+.field.alternative-name .fieldvalue:empty::after {
+  content: '(N/A)';
+  font-style: italic;
+}
+
+.field.alternative-last-name::after
+{
+  content: ',';
+}
+
+
+.field.alternative-name::after {
+  content: ';';
+}
+
+.field.alternative-first-name {
+  padding-right: 0;
+}
+
+.field.alternative-last-name {
+  padding-right: 0;
+}
+
+.external-resources >>> a,
+.scrape-data >>> div[role=button]
+{
+  color: black;
+}
+
+
 </style>
