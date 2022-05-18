438bbd633bf3b43175dcefa2e947b6febff9d1a5
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-05-18 12:35:55 +0200

    Move type to datacontainer [skip ci]

--- src/util/lemmaimport/datacontainers.ts
+++ src/util/lemmaimport/datacontainers.ts
@@ -44,3 +44,12 @@ export class Data2D {
     }
 
 }
+
+import { NewLemmaRow } from "@/types/lemma";
+
+/**
+ * A Lemma Prototype before type-casting / formatting
+ */
+export type LemmaPrototype = {
+    [key in keyof NewLemmaRow]: string
+};
--- src/util/lemmaimport/types.ts
+++
@@ -1,8 +0,0 @@
-import { NewLemmaRow } from "@/types/lemma";
-
-/**
- * A Lemma Prototype before type-casting / formatting
- */
-export type LemmaPrototype = {
-    [key in keyof NewLemmaRow]: string
-};
