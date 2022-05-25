f2f694b5a8b27ca9d5fcb6637b7ef5bdad9980d9
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-05-25 10:07:02 +0200

    Adding Null Manager
    Tags: [skip ci], Issue #74

--- src/views/LemmaManager/LemmaImporter/LemmaFormatter.vue
+++ src/views/LemmaManager/LemmaImporter/LemmaFormatter.vue
@@ -1,21 +1,38 @@
 <template>
     <div class="lemma-formatter-container">
-
+        <v-expansion-panels>
+            <v-expansion-panel class="null-managment-row">
+                <v-expansion-panel-header>Null-Werte</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <null-manager
+                        :lemmaPrototypes="lemmaPrototypes"
+                        :preloadedNullValues="localOptions.nullValues"
+                        @options="localOptions.nullValues = $event"
+                        @data="lemmasPrototypesWithNullsAndRequiredFields = $event"
+                    />
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
     </div>
 </template>
 
 <script lang="ts">
 
 import { LemmaRow } from "@/types/lemma";
-import { LemmaPrototypeStringType } from "@/util/lemmaimport/datacontainers";
+import { LemmaPrototypeStringType, LemmaPrototypeRequiredFieldsType } from "@/util/lemmaimport/datacontainers";
 import { LemmaFormatterOptions, defautLemmaFormatterOptions } from "@/util/lemmaimport/options";
 import { Component, Prop, Vue } from "vue-property-decorator";
+import NullManager from "./NullManager.vue";
 
 
 /**
  * Takes LemmaPrototypes And Converts Them To LemmaRows
  */
-@Component
+@Component({
+    components: {
+        NullManager,  
+    },
+})
 export default class LemmaFormatter extends Vue {
 
     @Prop({required: true, default: Array}) lemmaPrototypes!: LemmaPrototypeStringType[];
@@ -26,6 +43,8 @@ export default class LemmaFormatter extends Vue {
     created() {
         this.localOptions = this.preloadedOptions;
     }
+
+    lemmasPrototypesWithNullsAndRequiredFields: LemmaPrototypeRequiredFieldsType[] = [];
 
     newLemmas: LemmaRow[] = [];
 
