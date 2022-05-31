6945d4f6e1561529df11d504e3d40170f66874bc
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-05-31 11:10:29 +0200

    Added Additional Nesting For Grid And Used AutoFormatting
    So commit now, that changes can be easily seen. [Skip ci]

--- src/views/LemmaManager/LemmaImporter/LemmaFormatter.vue
+++ src/views/LemmaManager/LemmaImporter/LemmaFormatter.vue
@@ -1,97 +1,110 @@
 <template>
-    <div class="lemma-formatter-container">
-        <v-expansion-panels multiple>
+  <div class="lemma-formatter-container">
+    <v-container>
+      <v-row class="lemma-formatting-options">
+        <v-col>
+          <v-expansion-panels multiple>
             <v-expansion-panel class="null-managment-row">
-                <v-expansion-panel-header>Null-Werte</v-expansion-panel-header>
-                <v-expansion-panel-content eager>
-                    <null-manager
-                        :lemmaPrototypes="lemmaPrototypes"
-                        :preloadedNullValues="localOptions.nullValues"
-                        @options="localOptions.nullValues = $event"
-                        @data="lemmasPrototypesWithNullsAndRequiredFields = $event"
-                    />
-                </v-expansion-panel-content>
+              <v-expansion-panel-header>Null-Werte</v-expansion-panel-header>
+              <v-expansion-panel-content eager>
+                <null-manager
+                  :lemmaPrototypes="lemmaPrototypes"
+                  :preloadedNullValues="localOptions.nullValues"
+                  @options="localOptions.nullValues = $event"
+                  @data="lemmasPrototypesWithNullsAndRequiredFields = $event"
+                />
+              </v-expansion-panel-content>
             </v-expansion-panel>
             <v-expansion-panel>
-                <v-expansion-panel-header>Datumsformattierung</v-expansion-panel-header>
-                <v-expansion-panel-content eager>
-                    <date-formatter 
-                        :lemmaPrototypes="lemmasPrototypesWithNullsAndRequiredFields"
-                        :preloadedDateFormatOption="localOptions.dateFormat"
-                        @data="dates = $event"
-                        @options="localOptions.dateFormat = $event"
-                    />
-                </v-expansion-panel-content>
+              <v-expansion-panel-header
+                >Datumsformattierung</v-expansion-panel-header
+              >
+              <v-expansion-panel-content eager>
+                <date-formatter
+                  :lemmaPrototypes="lemmasPrototypesWithNullsAndRequiredFields"
+                  :preloadedDateFormatOption="localOptions.dateFormat"
+                  @data="dates = $event"
+                  @options="localOptions.dateFormat = $event"
+                />
+              </v-expansion-panel-content>
             </v-expansion-panel>
             <v-expansion-panel>
-                <v-expansion-panel-header>Gender</v-expansion-panel-header>
-                <v-expansion-panel-content eager>
-                    <gender-mapper 
-                        :lemmaPrototypes="lemmasPrototypesWithNullsAndRequiredFields"
-                        :preloadedOptions="localOptions.genderMapping"
-                        @data="genders = $event"
-                        @options="localOptions.genderMapping = $event"
-                    />
-                </v-expansion-panel-content>
+              <v-expansion-panel-header>Gender</v-expansion-panel-header>
+              <v-expansion-panel-content eager>
+                <gender-mapper
+                  :lemmaPrototypes="lemmasPrototypesWithNullsAndRequiredFields"
+                  :preloadedOptions="localOptions.genderMapping"
+                  @data="genders = $event"
+                  @options="localOptions.genderMapping = $event"
+                />
+              </v-expansion-panel-content>
             </v-expansion-panel>
-        </v-expansion-panels>
-    </div>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
-
 import { NewLemmaRow } from "@/types/lemma";
-import { LemmaPrototypeStringType, LemmaPrototypeRequiredFieldsType, LemmaDates, LemmaGender } from "@/util/lemmaimport/datacontainers";
+import {
+  LemmaPrototypeStringType,
+  LemmaPrototypeRequiredFieldsType,
+  LemmaDates,
+  LemmaGender,
+} from "@/util/lemmaimport/datacontainers";
 import { mergeBuildNewLemmaRows } from "@/util/lemmaimport/dataconversion";
-import { LemmaFormatterOptions, defautLemmaFormatterOptions } from "@/util/lemmaimport/options";
-import { Component, Prop, Vue } from "vue-property-decorator";
+import {
+  LemmaFormatterOptions,
+  defautLemmaFormatterOptions,
+} from "@/util/lemmaimport/options";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import DateFormatter from "./DateFormatter.vue";
 import GenderMapper from "./GenderMapper.vue";
 import NullManager from "./NullManager.vue";
-
 
 /**
  * Takes LemmaPrototypes And Converts Them To LemmaRows
  */
 @Component({
-    components: {
-        NullManager,  
-        DateFormatter,
-        GenderMapper,
-    },
+  components: {
+    NullManager,
+    DateFormatter,
+    GenderMapper,
+  },
 })
 export default class LemmaFormatter extends Vue {
+  @Prop({ required: true, default: Array })
+  lemmaPrototypes!: LemmaPrototypeStringType[];
+  @Prop({ required: true }) preloadedOptions!: LemmaFormatterOptions;
 
-    @Prop({required: true, default: Array}) lemmaPrototypes!: LemmaPrototypeStringType[];
-    @Prop({required: true }) preloadedOptions!: LemmaFormatterOptions;
+  localOptions: LemmaFormatterOptions = defautLemmaFormatterOptions;
 
-    localOptions: LemmaFormatterOptions = defautLemmaFormatterOptions;
+  created() {
+    this.localOptions = this.preloadedOptions;
+  }
 
-    created() {
-        this.localOptions = this.preloadedOptions;
-    }
+  lemmasPrototypesWithNullsAndRequiredFields: LemmaPrototypeRequiredFieldsType[] =
+    [];
 
-    lemmasPrototypesWithNullsAndRequiredFields: LemmaPrototypeRequiredFieldsType[] = [];
+  dates: LemmaDates[] = [];
+  genders: LemmaGender[] = [];
 
-    dates: LemmaDates[] = [];
-    genders: LemmaGender[] = [];
+  get newLemmas(): NewLemmaRow[] {
+    return mergeBuildNewLemmaRows(
+      this.lemmasPrototypesWithNullsAndRequiredFields,
+      this.dates,
+      this.genders
+    );
+  }
 
-    get newLemmas(): NewLemmaRow[] {
-        return mergeBuildNewLemmaRows(
-            this.lemmasPrototypesWithNullsAndRequiredFields,
-            this.dates,
-            this.genders,
-        );
-    }
-
-    submit() {
-        this.$emit('data', this.newLemmas);
-        this.$emit('options', this.localOptions);
-        this.$emit('submit');
-    }
-
-
-
+  @Watch("newLemmas", { immediate: true, deep: false }) // Let the browser rest. Watch options deep should cover all.
+  @Watch("options", { immediate: true, deep: true })
+  submit() {
+    this.$emit("data", this.newLemmas);
+    this.$emit("options", this.localOptions);
+    this.$emit("submit");
+  }
 }
-
 </script>
