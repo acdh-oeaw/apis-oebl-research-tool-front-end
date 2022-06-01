4c2ae9504dcfdfc7b64a4eb805ca81e204f27860
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-06-01 14:58:06 +0200

    Option Managment Controller
    Save, load, list and add options into local storage.
    
    Tags: Issue #74, [skip ci]

---
+++ src/util/lemmaimport/optionmanagement.ts
@@ -0,0 +1,54 @@
+import { ImportOptions } from "./options";
+
+
+type ImportOptionsCollections = {
+    [name: string]: ImportOptions;
+};
+
+
+
+
+export class ImportOptionManager {
+
+    storageKey: string = 'lemmaImportOptions'; 
+    importOptionsCollections: ImportOptionsCollections = {}
+
+    constructor() {
+
+        const loadedString = localStorage.getItem(this.storageKey);
+
+        if (loadedString === null) {
+            this.importOptionsCollections = {};
+        } else {
+            this.importOptionsCollections = JSON.parse(loadedString);
+        }
+    }
+
+    save() {
+        localStorage.setItem(
+            this.storageKey,
+            JSON.stringify(
+                this.importOptionsCollections
+                )
+            );
+    }
+
+    listImportOptionsNames(): string[] {
+        return Object.keys(this.importOptionsCollections);
+    }
+
+    getImportOptionByName(name: string): ImportOptions {
+        const options = this.importOptionsCollections[name];
+        if (options === undefined) {
+            throw new Error(`Can't find options with name ${name}`);
+        }
+
+        return options;
+    }
+
+    addOrUpdateImportOptions(name: string, options: ImportOptions) {
+        this.importOptionsCollections[name] = options;
+        this.save();
+    }
+    
+}
