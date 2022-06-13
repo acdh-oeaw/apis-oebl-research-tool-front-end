5a6933de97b573756883583b80690607c635af98
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-06-13 16:59:53 +0200

    Post Import To Server
    Tags: [skip ci]

--- src/util/lemmaimport/lemmaimport.ts
+++ src/util/lemmaimport/lemmaimport.ts
@@ -1,8 +1,42 @@
+import { LemmasCreateRequest, ResearchService } from "@/api";
 import { NewLemmaRow } from "@/types/lemma";
 
 
 export async function importLemmas(lemmas: NewLemmaRow[]): Promise<void> {
-    await new Promise(r => setTimeout(r, 1123)); // Work in progress: Fake network loading time in ux.
-    throw Error('Not implemented');
+
+    const lists = new Set(lemmas.map(lemma => lemma.list?.id));
+    if (lists.size !== 1) {
+        throw new Error('Can only import lemmas belonging to one list at a time');
+    }
+
+    const list = lemmas[0].list;
+
+    if (list === undefined) {
+        throw new Error('Can not import lemmas without a list');
+    }
+
+    const id = list.id;
+
+    if (id === undefined)  {
+        throw new Error('can not import with a list, without an ID');
+    }
+
+    const requestReadyLemmas: LemmasCreateRequest[] = lemmas.map(
+        lemma => Object.assign(
+            lemma, 
+            {
+                selected: false, // Now this makes sense, doesn't it?
+                dateOfBirth: lemma.dateOfBirth.generateISO_OnlyDate(),
+                dateOfDeath: lemma.dateOfDeath.generateISO_OnlyDate(),
+            }
+        )
+    );
+
+    await ResearchService.researchApiV1LemmaresearchCreate(
+        {
+            listId: id,
+            lemmas: requestReadyLemmas,
+        }
+    );
 
 }
