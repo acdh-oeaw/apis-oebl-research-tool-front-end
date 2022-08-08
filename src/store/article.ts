71e0a728a85bb0466a23681927512d457283503d
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-08-08 12:20:33 +0200

    Article Store API
    Tags: [skip ci]

--- src/store/article.ts
+++ src/store/article.ts
@@ -1,7 +1,47 @@
+import { LemmaArticleVersion } from "@/api";
 
 
-export default class ArticleStore {
 
+
+
+export interface ArticleStoreInterface {
+
+
+    get versions(): LemmaArticleVersion[];
+
+    /**
+     * The newest version of an article is undefined, if there is no version
+     */
+    get newestVersion(): LemmaArticleVersion|undefined;
+
+    /**
+     * Updates tthe markup of the newest version, if there is one.
+     * 
+     * @param new_markup 
+     */
+    updateMarkup(new_markup: LemmaArticleVersion['markup']): Promise<ArticleStoreInterface>;
+
+    addVersion(new_markup: LemmaArticleVersion['markup']): Promise<ArticleStoreInterface>;
+
+}
+
+export async function loadArticle(article_id: number): Promise<ArticleStoreInterface> {
+    throw new Error('Not iemplemented');
+}
+
+export class ArticleStore implements ArticleStoreInterface {
+    get versions(): LemmaArticleVersion[] {
+        throw new Error("Method not implemented.");
+    }
+    get newestVersion(): LemmaArticleVersion | undefined {
+        throw new Error("Method not implemented.");
+    }
+    updateMarkup(new_markup: Record<string, any>): Promise<ArticleStoreInterface> {
+        throw new Error("Method not implemented.");
+    }
+    addVersion(new_markup: Record<string, any>): Promise<ArticleStoreInterface> {
+        throw new Error("Method not implemented.");
+    }
 
 
 }
