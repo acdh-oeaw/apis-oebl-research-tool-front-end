ab66154177f0ca50be1c0a09b806b23b0821fafc
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-08-08 13:23:42 +0200

    Implementation Of Article Store Object Interface
    Tags: [skip ci]

--- src/store/article.ts
+++ src/store/article.ts
@@ -1,18 +1,23 @@
 import { LemmaArticleVersion } from "@/api";
+import { EditorService } from "@/api/services/EditorService";
 
 
+/**
+ * Server side versions have default values for this properties and are not nullable 
+ */
+type SavedArticleVersion = LemmaArticleVersion & { date_created: string, date_modified: string, id: number };
 
 
 
 export interface ArticleStoreInterface {
 
 
-    get versions(): LemmaArticleVersion[];
+    get versions(): SavedArticleVersion[];
 
     /**
      * The newest version of an article is undefined, if there is no version
      */
-    get newestVersion(): LemmaArticleVersion|undefined;
+    get newestVersion(): SavedArticleVersion | undefined;
 
     /**
      * Updates tthe markup of the newest version, if there is one.
@@ -29,19 +34,52 @@ export async function loadArticle(article_id: number): Promise<ArticleStoreInter
     throw new Error('Not iemplemented');
 }
 
+
+
+
 export class ArticleStore implements ArticleStoreInterface {
-    get versions(): LemmaArticleVersion[] {
-        throw new Error("Method not implemented.");
-    }
-    get newestVersion(): LemmaArticleVersion | undefined {
-        throw new Error("Method not implemented.");
-    }
-    updateMarkup(new_markup: Record<string, any>): Promise<ArticleStoreInterface> {
-        throw new Error("Method not implemented.");
-    }
-    addVersion(new_markup: Record<string, any>): Promise<ArticleStoreInterface> {
-        throw new Error("Method not implemented.");
+
+    constructor(
+        private article_id: number,
+        private _versions: SavedArticleVersion[],
+    ) { }
+
+
+    get versions(): SavedArticleVersion[] {
+        return this._versions;
     }
 
+    get newestVersion(): SavedArticleVersion | undefined {
+
+        if (this.versions.length === 0) {
+            return undefined;
+        }
+
+        return this.versions.reduce(
+            (version1, version2) => new Date(version1.date_created) > new Date(version2.date_created) ? version1 : version2
+        );
+
+    }
+
+    async updateMarkup(new_markup: Record<string, any>): Promise<ArticleStoreInterface> {
+        const newestVersion = this.newestVersion;
+        if (newestVersion === undefined) {
+            throw new Error('Can not update markup – newest version not found');
+        }
+
+        const index = this.versions.indexOf(newestVersion);
+
+        const updatedVersion = await EditorService.editorApiV1LemmaArticleVersionPartialUpdate(newestVersion.id, { markup: new_markup }) as SavedArticleVersion;
+
+        this._versions[index] = updatedVersion;
+
+        return this;
+    }
+
+    async addVersion(new_markup: Record<string, any>): Promise<ArticleStoreInterface> {
+        const newVersion = await EditorService.editorApiV1LemmaArticleVersionCreate({ lemma_article: this.article_id, markup = new_markup }) as SavedArticleVersion;
+        this.versions.push(newVersion);
+        return this;
+    }
 
 }
