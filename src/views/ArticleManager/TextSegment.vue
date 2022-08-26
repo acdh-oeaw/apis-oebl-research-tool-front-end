f428875293e2436d512a649c7c459df6e0fd01e0
Philip Röggla <philip.roeggla@oeaw.ac.at>
2022-08-26 17:48:16 +0200

    Select Text Segment Tag: h1 / h2 / p
    Tags: [skip ci]

--- src/views/ArticleManager/TextSegment.vue
+++ src/views/ArticleManager/TextSegment.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class='TextSegment-container'>
-        <component :is="annotatedTextSequenceStore.tag">
-            <annotated-text-sequence-form-element 
-                v-for="(textSequence, key) in annotatedTextSequenceStore.textSequences" 
-                :key="key"
-                :annotatedTextSequence="textSequence"
-                :canEdit="canEdit"
-                @[annotatedTextSequenceSelectEventType]="$emit(annotatedTextSequenceSelectEventType, $event);"
-            >
-            </annotated-text-sequence-form-element>
-        </component>
+    <div class='TextSegment-container' @click="addEmptyTextSequence">
+        <v-container>
+            <v-row dense>
+                <v-col cols="11">
+                    <v-sheet elevation="9">
+                        <component :is="annotatedTextSequenceStore.tag" >
+                        <annotated-text-sequence-form-element 
+                            v-for="(textSequence, key) in annotatedTextSequenceStore.textSequences" 
+                            :key="key"
+                            :annotatedTextSequence="textSequence"
+                            :canEdit="canEdit"
+                            @[annotatedTextSequenceSelectEventType]="$emit(annotatedTextSequenceSelectEventType, $event);"
+                        >
+                        </annotated-text-sequence-form-element>
+                    </component>
+                    </v-sheet>
+                </v-col>
+                <v-col cols="0.5">
+                    <v-select :items="[
+                        {text: 'Absatz', value: 'p'},
+                        {text: 'Überschrift 1', value: 'h1'},
+                        {text: 'Überschrift 2', value: 'h2'},
+                    ]"
+                    v-model="annotatedTextSequenceStore.tag"
+
+                    />
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-divider/>
     </div>
 </template>
 
@@ -33,6 +52,13 @@ export default class TextSegment extends Vue {
      * To pass events to parent in template
      */
     annotatedTextSequenceSelectEventType = AnnotatedTextSequenceSelectEventType;
+
+    addEmptyTextSequence() {
+        // Make sure, there is something to edit
+        if (this.annotatedTextSequenceStore.textSequences.length === 0) {
+            this.annotatedTextSequenceStore.addEmptyTextSequence();
+        }
+    }
 
 }
 
