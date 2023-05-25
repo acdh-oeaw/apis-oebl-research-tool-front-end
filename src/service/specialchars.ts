5be6d199e66f2bc723134803a66cf67bdeb14d5b
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2023-05-25 14:49:33 +0200

    add files with special chars

---
+++ src/service/specialchars.ts
@@ -0,0 +1,26 @@
+export const specialChar = {
+  groups: [
+    {
+      group: 'Pfeile',
+        chars: [
+            [ 8592, 'leftwards arrow' ],
+            [ 8593, 'upwards arrow' ],
+            [ 8594, 'rightwards arrow' ],
+            [ 8595, 'downwards arrow' ],
+            [ 8596, 'left right arrow' ],
+            [ 8629, 'carriage return' ],
+            [ 8656, 'leftwards double arrow' ],
+            [ 8657, 'upwards double arrow' ],
+            [ 8658, 'rightwards double arrow' ],
+            [ 8659, 'downwards double arrow' ],
+            [ 8660, 'left right double arrow' ],
+          ]
+        },
+    { 
+      group: 'Genealogie',
+        chars: [
+          [ 8224,'dagger' ],
+        ]
+      }
+    ]
+  }
