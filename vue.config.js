bcfa700d0fdd066885503a03feb47a82b0954954
Stefan Probst <stefanprobst@posteo.de>
2023-06-30 12:39:37 +0200

    chore: handle .js files with tsc

--- vue.config.js
+++ vue.config.js
@@ -1,52 +1,49 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
-  lintOnSave: false,
-  transpileDependencies: [
-    'vuetify',
-    'get-stream'
-  ],
-  runtimeCompiler: true,
-  configureWebpack: (config) => {
-    if (
-      process.env.NODE_ENV !== 'production'
-    ) {
-      config
-        .plugins
-        .push(new BundleAnalyzerPlugin({
-          defaultSizes: 'gzip'
-        }))
-    }
-    config.module.rules.unshift({
-      test: /\.worker\.ts$/,
-      use: [
-        {
-          loader: 'worker-loader',
-          options: {
-            inline: 'no-fallback'
-          }
-        }
-      ]
-    })
-    config.module.rules.unshift({
-      test: require('path').resolve(__dirname, 'node_modules/leader-line/'),
-      loader: 'skeleton-loader',
-      options: { procedure: content => `${content} export default LeaderLine` }
-    })
-  },
-  chainWebpack: (config) => {
-    config.plugins.delete('prefetch')
-  },
-  parallel: true,
-  devServer: {
-    open: 'Google Chrome',
-    // https: true,
-    // writeToDisk: true,
-    disableHostCheck: true,
-    host: 'localhost',
-    port: '8080',
-    watchOptions: {
-      poll: false
-    }
-  }
-}
+const config = {
+	lintOnSave: false,
+	transpileDependencies: ["vuetify", "get-stream"],
+	runtimeCompiler: true,
+	configureWebpack: (config) => {
+		if (process.env.NODE_ENV !== "production") {
+			config.plugins.push(
+				new BundleAnalyzerPlugin({
+					defaultSizes: "gzip",
+				}),
+			);
+		}
+		config.module.rules.unshift({
+			test: /\.worker\.ts$/,
+			use: [
+				{
+					loader: "worker-loader",
+					options: {
+						inline: "no-fallback",
+					},
+				},
+			],
+		});
+		config.module.rules.unshift({
+			test: require("path").resolve(__dirname, "node_modules/leader-line/"),
+			loader: "skeleton-loader",
+			options: { procedure: (content) => `${content} export default LeaderLine` },
+		});
+	},
+	chainWebpack: (config) => {
+		config.plugins.delete("prefetch");
+	},
+	parallel: true,
+	devServer: {
+		open: "Google Chrome",
+		// https: true,
+		// writeToDisk: true,
+		disableHostCheck: true,
+		host: "localhost",
+		port: "8080",
+		watchOptions: {
+			poll: false,
+		},
+	},
+};
+
+module.exports = config;
