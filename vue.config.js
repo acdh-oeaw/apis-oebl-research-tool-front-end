87086f82899c233331eacf7094016fc0863926ca
Stefan Probst <stefanprobst@posteo.de>
2023-07-02 08:42:27 +0200

    build: disable type checking via webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 const config = defineConfig({
-	lintOnSave: false,
-	transpileDependencies: ["vuetify", "get-stream"],
-	runtimeCompiler: true,
+	chainWebpack: (config) => {
+		config.plugins.delete("prefetch");
+	},
 	configureWebpack: (config) => {
+		/** Disable typechecking via webpack plugin. */
+		config.plugins = config.plugins.filter((p) => !(p instanceof ForkTsCheckerWebpackPlugin));
+
 		if (process.env.NODE_ENV !== "production") {
 			config.plugins.push(
 				new BundleAnalyzerPlugin({
@@ -13,6 +17,7 @@ const config = defineConfig({
 				}),
 			);
 		}
+
 		config.module.rules.unshift({
 			test: /\.worker\.ts$/,
 			use: [
@@ -24,20 +29,19 @@ const config = defineConfig({
 				},
 			],
 		});
+
 		config.module.rules.unshift({
 			test: require("path").resolve(__dirname, "node_modules/leader-line/"),
 			loader: "skeleton-loader",
 			options: { procedure: (content) => `${content} export default LeaderLine` },
 		});
 	},
-	chainWebpack: (config) => {
-		config.plugins.delete("prefetch");
-	},
-	parallel: true,
 	devServer: {
 		compress: true,
 		port: 8080,
 	},
+	runtimeCompiler: true,
+	transpileDependencies: ["vuetify", "get-stream"],
 });
 
 module.exports = config;
