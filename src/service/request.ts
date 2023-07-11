42a91b0c529cd354f6d6948d79b112da7f38c3cc
Stefan Probst <stefanprobst@posteo.de>
2023-07-11 15:43:55 +0200

    revert: pin openapi-typescript-codegen version for now

--- src/service/request.ts
+++ src/service/request.ts
@@ -3,16 +3,23 @@
 /* tslint:disable */
 /* eslint-disable */
 import { ApiError } from "@/api/core/ApiError";
-import type { ApiRequestOptions } from "@/api/core/ApiRequestOptions";
-import type { ApiResult } from "@/api/core/ApiResult";
-import { CancelablePromise } from "@/api/core/CancelablePromise";
-import type { OnCancel } from "@/api/core/CancelablePromise";
-import type { OpenAPIConfig } from "@/api/core/OpenAPI";
+import { type ApiRequestOptions } from "@/api/core/ApiRequestOptions";
+import { type ApiResult } from "@/api/core/ApiResult";
+import { OpenAPI } from "@/api/core/OpenAPI";
 
 import store from "@/store";
 import confirm from "@/store/confirm";
 
-// FIXME: this was added
+/**
+ * These were added or changed compared to the upstream request module:
+ * - `requestState` added
+ * - `beforeunload` event added
+ * - token authentication changed to use "Token" prefix instead of "Bearer" to
+ *   align with django restframework
+ * - active request counting in `request` funtion
+ * - changes to error handling in `request` function
+ */
+
 export const requestState = {
 	countWriteRequests: 0,
 	countRequests: 0,
@@ -22,273 +29,176 @@ export const requestState = {
 	},
 };
 
-// FIXME: this was added
-function warnBeforeLeave(e: BeforeUnloadEvent): string | undefined {
+function warnBeforeLeave(event: BeforeUnloadEvent): string | undefined {
 	if (requestState.countWriteRequests > 0) {
 		const confirmationMessage =
 			"Synchronisierung läuft noch. Beim Beenden können Änderungen verloren gehen. Wirklich beenden?";
-		e.returnValue = confirmationMessage;
+		event.returnValue = confirmationMessage;
 		return confirmationMessage;
 	}
 }
 
-// FIXME: this was added
 window.addEventListener("beforeunload", warnBeforeLeave);
 
-export const isDefined = <T>(
-	value: T | null | undefined,
-): value is Exclude<T, null | undefined> => {
+//
+
+function isDefined<T>(value: T | null | undefined): value is Exclude<T, null | undefined> {
 	return value !== undefined && value !== null;
-};
+}
 
-export const isString = (value: any): value is string => {
+function isString(value: any): value is string {
 	return typeof value === "string";
-};
+}
 
-export const isStringWithValue = (value: any): value is string => {
+function isStringWithValue(value: any): value is string {
 	return isString(value) && value !== "";
-};
+}
 
-export const isBlob = (value: any): value is Blob => {
-	return (
-		typeof value === "object" &&
-		typeof value.type === "string" &&
-		typeof value.stream === "function" &&
-		typeof value.arrayBuffer === "function" &&
-		typeof value.constructor === "function" &&
-		typeof value.constructor.name === "string" &&
-		/^(Blob|File)$/.test(value.constructor.name) &&
-		/^(Blob|File)$/.test(value[Symbol.toStringTag])
-	);
-};
+function isBlob(value: any): value is Blob {
+	return value instanceof Blob;
+}
 
-export const isFormData = (value: any): value is FormData => {
-	return value instanceof FormData;
-};
-
-export const base64 = (str: string): string => {
-	try {
-		return btoa(str);
-	} catch (err) {
-		// @ts-ignore
-		return Buffer.from(str).toString("base64");
-	}
-};
-
-export const getQueryString = (params: Record<string, any>): string => {
+function getQueryString(params: Record<string, any>): string {
 	const qs: string[] = [];
-
-	const append = (key: string, value: any) => {
-		qs.push(`${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`);
-	};
-
-	const process = (key: string, value: any) => {
+	Object.keys(params).forEach((key) => {
+		const value = params[key];
 		if (isDefined(value)) {
 			if (Array.isArray(value)) {
-				value.forEach((v) => {
-					process(key, v);
-				});
-			} else if (typeof value === "object") {
-				Object.entries(value).forEach(([k, v]) => {
-					process(`${key}[${k}]`, v);
+				value.forEach((value) => {
+					qs.push(`${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`);
 				});
 			} else {
-				append(key, value);
+				qs.push(`${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`);
 			}
 		}
-	};
-
-	Object.entries(params).forEach(([key, value]) => {
-		process(key, value);
 	});
-
 	if (qs.length > 0) {
 		return `?${qs.join("&")}`;
 	}
-
 	return "";
-};
+}
 
-const getUrl = (config: OpenAPIConfig, options: ApiRequestOptions): string => {
-	const encoder = config.ENCODE_PATH || encodeURI;
+function getUrl(options: ApiRequestOptions): string {
+	const path = options.path.replace(/[:]/g, "_");
+	const url = `${OpenAPI.BASE}${path}`;
 
-	const path = options.url
-		.replace("{api-version}", config.VERSION)
-		.replace(/{(.*?)}/g, (substring: string, group: string) => {
-			if (options.path?.hasOwnProperty(group)) {
-				return encoder(String(options.path[group]));
-			}
-			return substring;
-		});
-
-	const url = `${config.BASE}${path}`;
 	if (options.query) {
 		return `${url}${getQueryString(options.query)}`;
 	}
 	return url;
-};
+}
 
-export const getFormData = (options: ApiRequestOptions): FormData | undefined => {
-	if (options.formData) {
-		const formData = new FormData();
-
-		const process = (key: string, value: any) => {
-			if (isString(value) || isBlob(value)) {
-				formData.append(key, value);
-			} else {
-				formData.append(key, JSON.stringify(value));
-			}
-		};
-
-		Object.entries(options.formData)
-			.filter(([_, value]) => isDefined(value))
-			.forEach(([key, value]) => {
-				if (Array.isArray(value)) {
-					value.forEach((v) => process(key, v));
-				} else {
-					process(key, value);
-				}
-			});
-
-		return formData;
-	}
-	return undefined;
-};
+function getFormData(params: Record<string, any>): FormData {
+	const formData = new FormData();
+	Object.keys(params).forEach((key) => {
+		const value = params[key];
+		if (isDefined(value)) {
+			formData.append(key, value);
+		}
+	});
+	return formData;
+}
 
 type Resolver<T> = (options: ApiRequestOptions) => Promise<T>;
 
-export const resolve = async <T>(
+async function resolve<T>(
 	options: ApiRequestOptions,
 	resolver?: T | Resolver<T>,
-): Promise<T | undefined> => {
+): Promise<T | undefined> {
 	if (typeof resolver === "function") {
 		return (resolver as Resolver<T>)(options);
 	}
 	return resolver;
-};
+}
 
-export const getHeaders = async (
-	config: OpenAPIConfig,
-	options: ApiRequestOptions,
-): Promise<Headers> => {
-	const token = await resolve(options, config.TOKEN);
-	const username = await resolve(options, config.USERNAME);
-	const password = await resolve(options, config.PASSWORD);
-	const additionalHeaders = await resolve(options, config.HEADERS);
+async function getHeaders(options: ApiRequestOptions): Promise<Headers> {
+	const token = await resolve(options, OpenAPI.TOKEN);
+	const username = await resolve(options, OpenAPI.USERNAME);
+	const password = await resolve(options, OpenAPI.PASSWORD);
+	const defaultHeaders = await resolve(options, OpenAPI.HEADERS);
 
-	const headers = Object.entries({
+	const headers = new Headers({
 		Accept: "application/json",
-		...additionalHeaders,
+		...defaultHeaders,
 		...options.headers,
-	})
-		.filter(([_, value]) => isDefined(value))
-		.reduce(
-			(headers, [key, value]) => ({
-				...headers,
-				[key]: String(value),
-			}),
-			{} as Record<string, string>,
-		);
+	});
 
 	if (isStringWithValue(token)) {
-		/** FIXME: This was changed to work with django rest framework. */
-		headers["Authorization"] = `Token ${token}`;
+		console.log(`token ${token}`);
+		headers.append("Authorization", `Token ${token}`);
 	}
 
 	if (isStringWithValue(username) && isStringWithValue(password)) {
-		const credentials = base64(`${username}:${password}`);
-		headers["Authorization"] = `Basic ${credentials}`;
+		const credentials = btoa(`${username}:${password}`);
+		headers.append("Authorization", `Basic ${credentials}`);
 	}
 
 	if (options.body) {
-		if (options.mediaType) {
-			headers["Content-Type"] = options.mediaType;
-		} else if (isBlob(options.body)) {
-			headers["Content-Type"] = options.body.type || "application/octet-stream";
+		if (isBlob(options.body)) {
+			headers.append("Content-Type", options.body.type || "application/octet-stream");
 		} else if (isString(options.body)) {
-			headers["Content-Type"] = "text/plain";
-		} else if (!isFormData(options.body)) {
-			headers["Content-Type"] = "application/json";
+			headers.append("Content-Type", "text/plain");
+		} else {
+			headers.append("Content-Type", "application/json");
 		}
 	}
+	return headers;
+}
 
-	return new Headers(headers);
-};
-
-export const getRequestBody = (options: ApiRequestOptions): any => {
-	if (options.body !== undefined) {
-		if (options.mediaType?.includes("/json")) {
-			return JSON.stringify(options.body);
-		} else if (isString(options.body) || isBlob(options.body) || isFormData(options.body)) {
+function getRequestBody(options: ApiRequestOptions): BodyInit | undefined {
+	if (options.formData) {
+		return getFormData(options.formData);
+	}
+	if (options.body) {
+		if (isString(options.body) || isBlob(options.body)) {
 			return options.body;
 		} else {
 			return JSON.stringify(options.body);
 		}
 	}
 	return undefined;
-};
+}
 
-export const sendRequest = async (
-	config: OpenAPIConfig,
-	options: ApiRequestOptions,
-	url: string,
-	body: any,
-	formData: FormData | undefined,
-	headers: Headers,
-	onCancel: OnCancel,
-): Promise<Response> => {
-	const controller = new AbortController();
-
+async function sendRequest(options: ApiRequestOptions, url: string): Promise<Response> {
 	const request: RequestInit = {
-		headers,
-		body: body ?? formData,
 		method: options.method,
-		signal: controller.signal,
+		headers: await getHeaders(options),
+		body: getRequestBody(options),
 	};
-
-	if (config.WITH_CREDENTIALS) {
-		request.credentials = config.CREDENTIALS;
+	if (OpenAPI.WITH_CREDENTIALS) {
+		request.credentials = "include";
 	}
-
-	onCancel(() => controller.abort());
-
 	return await fetch(url, request);
-};
+}
 
-export const getResponseHeader = (
-	response: Response,
-	responseHeader?: string,
-): string | undefined => {
+function getResponseHeader(response: Response, responseHeader?: string): string | null {
 	if (responseHeader) {
 		const content = response.headers.get(responseHeader);
 		if (isString(content)) {
 			return content;
 		}
 	}
-	return undefined;
-};
+	return null;
+}
 
-export const getResponseBody = async (response: Response): Promise<any> => {
-	if (response.status !== 204) {
-		try {
-			const contentType = response.headers.get("Content-Type");
-			if (contentType) {
-				const jsonTypes = ["application/json", "application/problem+json"];
-				const isJSON = jsonTypes.some((type) => contentType.toLowerCase().startsWith(type));
-				if (isJSON) {
-					return await response.json();
-				} else {
-					return await response.text();
-				}
+async function getResponseBody(response: Response): Promise<any> {
+	try {
+		const contentType = response.headers.get("Content-Type");
+		if (contentType) {
+			const isJSON = contentType.toLowerCase().startsWith("application/json");
+			if (isJSON) {
+				return await response.json();
+			} else {
+				return await response.text();
 			}
-		} catch (error) {
-			console.error(error);
 		}
+	} catch (error) {
+		console.error(error);
 	}
-	return undefined;
-};
+	return null;
+}
 
-export const catchErrorCodes = (options: ApiRequestOptions, result: ApiResult): void => {
+function catchErrors(options: ApiRequestOptions, result: ApiResult): void {
 	const errors: Record<number, string> = {
 		400: "Bad Request",
 		401: "Unauthorized",
@@ -302,85 +212,76 @@ export const catchErrorCodes = (options: ApiRequestOptions, result: ApiResult): 
 
 	const error = errors[result.status];
 	if (error) {
-		throw new ApiError(options, result, error);
+		throw new ApiError(result, error);
 	}
 
 	if (!result.ok) {
-		const errorStatus = result.status ?? "unknown";
-		const errorStatusText = result.statusText ?? "unknown";
-		const errorBody = (() => {
-			try {
-				return JSON.stringify(result.body, null, 2);
-			} catch (e) {
-				return undefined;
-			}
-		})();
-
-		throw new ApiError(
-			options,
-			result,
-			`Generic Error: status: ${errorStatus}; status text: ${errorStatusText}; body: ${errorBody}`,
-		);
+		throw new ApiError(result, "Generic Error");
 	}
-};
+}
 
 /**
- * Request method
- * @param config The OpenAPI configuration object
- * @param options The request options from the service
- * @returns CancelablePromise<T>
+ * Request using fetch client
+ * @param options The request options from the the service
+ * @returns ApiResult
  * @throws ApiError
  */
-export const request = <T>(
-	config: OpenAPIConfig,
-	options: ApiRequestOptions,
-): CancelablePromise<T> => {
-	return new CancelablePromise(async (resolve, reject, onCancel) => {
-		// FIXME: this was added - is that actually needed?
-		if (["post", "put", "patch", "delete"].includes(String(options.method).toLowerCase())) {
-			requestState.countWriteRequests++;
-		}
-		requestState.countRequests++;
+export async function request(options: ApiRequestOptions): Promise<ApiResult> {
+	const isWriteRequest = ["delete", "patch", "post", "put"].includes(options.method.toLowerCase());
 
-		try {
-			const url = getUrl(config, options);
-			const formData = getFormData(options);
-			const body = getRequestBody(options);
-			const headers = await getHeaders(config, options);
+	if (isWriteRequest) {
+		requestState.countWriteRequests++;
+	}
+	requestState.countRequests++;
 
-			if (!onCancel.isCancelled) {
-				const response = await sendRequest(config, options, url, body, formData, headers, onCancel);
-				const responseBody = await getResponseBody(response);
-				const responseHeader = getResponseHeader(response, options.responseHeader);
+	//
 
-				const result: ApiResult = {
-					url,
-					ok: response.ok,
-					status: response.status,
-					statusText: response.statusText,
-					body: responseHeader ?? responseBody,
-				};
+	const url = getUrl(options);
+	const response = await sendRequest(options, url);
+	const responseBody = await getResponseBody(response);
+	const responseHeader = getResponseHeader(response, options.responseHeader);
 
-				catchErrorCodes(options, result);
+	const result: ApiResult = {
+		url,
+		ok: response.ok,
+		status: response.status,
+		statusText: response.statusText,
+		body: responseHeader || responseBody,
+	};
 
-				resolve(result.body);
-			}
-		} catch (error) {
-			// FIXME: this was added
-			if (error instanceof ApiError && error.status === 401) {
-				store.isLoggedIn = false;
-				// TODO: this added a callback which would run after login
-			}
+	//
 
-			// FIXME: this was added
+	if (isWriteRequest) {
+		requestState.countWriteRequests--;
+	}
+	requestState.countRequests--;
+
+	//
+
+	if (result.ok) {
+		return result;
+	} else {
+		if (result.status === 401) {
+			store.isLoggedIn = false;
+
+			console.log("Unauthorized Access. Waiting for log-in before continuing.");
+
+			return new Promise((resolve, reject) => {
+				store.onLoginSuccess(async () => {
+					return request(options).then(resolve).catch(reject);
+				});
+			});
+		} else {
 			requestState.hasErrored = true;
-			console.error(error);
-			confirm.confirm("Serverfehler. Details in der Console.", {
+
+			console.error(result);
+
+			await confirm.confirm("Serverfehler. Details in der Console.", {
 				showCancel: false,
 				icon: "mdi-error",
 			});
 
-			reject(error);
+			return result;
 		}
-	});
-};
+	}
+}
